b47bde2e5c0c596aaf3e3dd7cfd62a13482c64ce
Luis Altuve <luis2ra@gmail.com>
2022-10-14 18:04:45 -0500

    class 14: conditionals in python

---
+++ 14-condicionales.py
@@ -0,0 +1,22 @@
+# class 14: Condicionales en Python
+
+'''
+# uso de la sentencias if / else
+
+edad = int(input("Escribe tu edad: "))
+if edad > 17:
+   print('Eres mayor de edad')
+else:
+    print('Eres menor de edad')
+
+'''
+
+# uso de la sentencia elif
+numero = int(input('Escribe un número: '))
+
+if numero > 5:
+    print('Es mayor a 5')
+elif numero == 5:
+    print('Es igual a 5')
+else:
+    print('Es menor a 5')
